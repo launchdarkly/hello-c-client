6e93af417cfabd03a78f49453207bdeef0db04ed
Andrew Shannon Brown <ashanbrown@users.noreply.github.com>
2018-10-29 12:49:04 -0700

    Initial version (#1)

---
+++ Makefile
@@ -0,0 +1,19 @@
+C_CLIENT_PATH = c-client
+C_CLIENT_REPO = https://github.com/launchdarkly/c-client
+CC = gcc -std=c99
+CFLAGS = -g -Wall -I$(C_CLIENT_PATH)
+LFLAGS = $(C_CLIENT_PATH)/libldapi.a -lcurl -lpthread -lm
+
+TARGET = hello
+
+all: $(TARGET)
+
+clean:
+	rm $(TARGET)
+
+$(TARGET): $(TARGET).c c-client
+	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c $(LFLAGS)
+
+c-client:
+	git clone $(C_CLIENT_REPO) $(C_CLIENT_PATH)
+	cd c-client && make
---
+++ hello.c
@@ -0,0 +1,47 @@
+#include "unistd.h"
+#include "signal.h"
+#include "stdbool.h"
+#include "stdio.h"
+
+#include "ldapi.h"
+
+#define YOUR_MOBILE_SDK_KEY "<put your mobile sdk key here>"
+#define YOUR_FEATURE_KEY "<put your feature key here>"
+
+void logger(const char *s) {
+    printf("LD: %s", s);
+}
+
+void on_signal(int signum) {
+   // tear down the client
+   LDClientClose(LDClientGet());
+   exit(0);
+}
+
+int main() {
+    LDSetLogFunction(LD_LOG_INFO, logger);
+    LDConfig *config = LDConfigNew(YOUR_MOBILE_SDK_KEY);
+    char* key = "bob@example.com";
+    LDUser *user = LDUserNew(key);
+    LDClient *client = LDClientInit(config, user);
+    LDClientAwaitInitialized(client, 3000); // wait up to 3 seconds to connect
+
+    signal(SIGINT, on_signal);
+
+    // disable output buffering
+    setbuf(stdout, NULL);
+
+    while (1) {
+        int show_feature = LDBoolVariation(client, YOUR_FEATURE_KEY, false);
+
+        if (show_feature) {
+            // application code to show the feature
+            printf("Showing your feature to %s\n", key);
+        } else {
+            // the code to run if the feature is off
+            printf("Not showing your feature to %s\n", key);
+        }
+
+        sleep(1);
+    }
+}
