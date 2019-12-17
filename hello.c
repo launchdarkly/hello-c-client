f4029ff8374db0c92f19f9732d7d128aecffdf8b
hroederld <hroeder@launchdarkly.com>
2019-12-17 15:45:35 -0800

    [ch59507] hello app improvements (#4)

--- hello.c
+++ hello.c
@@ -1,21 +1,14 @@
-#include "unistd.h"
-#include "signal.h"
-#include "stdbool.h"
-#include "stdio.h"
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "ldapi.h"
+#include "uthash.h"
 
 #define YOUR_MOBILE_SDK_KEY "<put your mobile sdk key here>"
 #define YOUR_FEATURE_KEY "<put your feature key here>"
 
 void logger(const char *s) {
     printf("LD: %s\n", s);
-}
-
-void on_signal(int signum) {
-    // tear down the client
-    LDClientClose(LDClientGet());
-    exit(0);
 }
 
 int main() {
@@ -27,22 +20,17 @@ int main() {
     // wait up to 3 seconds to connect
     LDClient *client = LDClientInit(config, user, 3000);
 
-    signal(SIGINT, on_signal);
+    int show_feature = LDBoolVariation(client, YOUR_FEATURE_KEY, false);
 
-    // disable output buffering
-    setbuf(stdout, NULL);
-
-    while (1) {
-        int show_feature = LDBoolVariation(client, YOUR_FEATURE_KEY, false);
-
-        if (show_feature) {
-            // application code to show the feature
-            printf("Showing your feature to %s\n", key);
-        } else {
-            // the code to run if the feature is off
-            printf("Not showing your feature to %s\n", key);
-        }
-
-        sleep(1);
+    if (show_feature) {
+        // application code to show the feature
+        printf("Showing your feature to %s\n", key);
+    } else {
+        // the code to run if the feature is off
+        printf("Not showing your feature to %s\n", key);
     }
+
+    LDClientClose(client);
+
+    return 0;
 }
