57af8a0bc8cc5640b75a7d55f1ba632a9dd4f080
Harpo Roeder <hroeder@launchdarkly.com>
2019-12-17 15:29:18 -0800

    complete platform download scripts and use them in circleci

--- fetch.ps1
+++
@@ -1,7 +0,0 @@
-# script from https://blog.markvincze.com/download-artifacts-from-a-latest-github-release-in-sh-and-powershell/
-$latestRelease = Invoke-WebRequest https://github.com/account/project/releases/latest -Headers @{"Accept"="application/json"}
-$json = $latestRelease.Content | ConvertFrom-Json
-$latestVersion = $json.tag_name
-$url = "https://github.com/account/project/releases/download/$latestVersion/myArtifact.zip"
-Invoke-WebRequest -Uri $url
-Expand-Archive -Path artifacts\windows-vs-64bit-dynamic.zip -DestinationPath .
