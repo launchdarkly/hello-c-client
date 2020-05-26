bf90b232bc0868cf7d4864e986e0cc2bdef3f099
BogdanZavu <zavu_bogdan@yahoo.com>
2020-05-26 11:35:16 -0400

    update sample

--- hello.c
+++ hello.c
@@ -6,22 +6,26 @@
 
 #define YOUR_MOBILE_SDK_KEY "<put your mobile sdk key here>"
 #define YOUR_FEATURE_KEY "<put your feature key here>"
-
+ 
 void logger(const char *s) {
     printf("LD: %s\n", s);
 }
 
 int main() {
     LDSetLogFunction(LD_LOG_INFO, logger);
-
+ 
     LDConfig *config = LDConfigNew(YOUR_MOBILE_SDK_KEY);
     const char* key = "bob@example.com";
     LDUser *user = LDUserNew(key);
+ 
     // wait up to 3 seconds to connect
-    LDClient_i *client = LDClientInit(config, user, 3000);
+    LDClient *client = LDClient::Init(config, user, 3000);
+ 
+    int show_feature = client->boolVariation(YOUR_FEATURE_KEY, false);
 
-    int show_feature = LDBoolVariation(client, YOUR_FEATURE_KEY, false);
-
+	client->close();
+	
+	
     if (show_feature) {
         // application code to show the feature
         printf("Showing your feature to %s\n", key);
@@ -29,8 +33,6 @@ int main() {
         // the code to run if the feature is off
         printf("Not showing your feature to %s\n", key);
     }
-
-    LDClientClose(client);
-
+ 
     return 0;
 }
