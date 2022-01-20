d285b9c8016f760dc6fe3c16d4ed952e64ab132b
Casey Waldren <cwaldren@launchdarkly.com>
2022-01-20 14:39:29 -0800

    Update build-mac.sh to allow for x86_64 compilation on Apple Silicon(#10)

--- build-mac.sh
+++ build-mac.sh
@@ -1,1 +1,1 @@
-clang -o hello hello.c lib/libldclientapi.a -I include -lcurl -lpthread -lm -framework CoreFoundation -framework IOKit
+clang -target x86_64-apple-darwin21.1.0 -o hello hello.c lib/libldclientapi.a -I include -lcurl -lpthread -lm -framework CoreFoundation -framework IOKit
